c1a4e88ff50f64b21fb9c02fe216ff555d26345d
fisher <fisher377164@gmail.com>
2016-04-11 05:24:44 +0300

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,6 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
-// var rename = require('gulp-rename');
-// var concat = require('gulp-concat');
-// var uglify = require('gulp-uglify');
-
-// gulp.task('scripts', function() {
-//     gulp.src(['./lib/file3.js', './lib/file1.js', './lib/file2.js'])
-//         .pipe(concat('all.js'))
-//         .pipe(uglify())
-//         .pipe(gulp.dest('./dist/'))
-// });
-
-
-// gulp.task('watch', function () {
-//     gulp.watch('./src/main/webapp/**/*.js', gulp.series('scripts'));
-//
-// });
-
 
 gulp.task('serve', function () {
     browserSync.init({
@@ -30,10 +13,8 @@ gulp.task('serve', function () {
 });
 
 gulp.task('default', gulp.series(
-    // 'scripts',
  
     gulp.parallel(
-        // 'watch',
         'serve'
     )
 ));
